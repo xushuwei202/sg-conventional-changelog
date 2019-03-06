84ed742d56b5176c5da8ef8cad8b52345f09adfb
xushuwei <xushuwei@sogou-inc.com>
2019-03-06 20:57:00 +0800

    增加自定义功能

--- engine.js
+++ engine.js
@@ -1,15 +1,8 @@
 "format cjs";
 
-var wrap = require('word-wrap');
 var map = require('lodash.map');
 var longest = require('longest');
 var rightPad = require('right-pad');
-
-var filter = function(array) {
-  return array.filter(function(x) {
-    return x;
-  });
-};
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
@@ -53,80 +46,18 @@ module.exports = function (options) {
           type: 'list',
           name: 'type',
           message: 'Select the type of change that you\'re committing:',
-          choices: choices,
-          default: options.defaultType
-        }, {
-          type: 'input',
-          name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n',
-          default: options.defaultScope
+          choices: choices
         }, {
           type: 'input',
           name: 'subject',
           message: 'Write a short, imperative tense description of the change:\n',
           default: options.defaultSubject
-        }, {
-          type: 'input',
-          name: 'body',
-          message: 'Provide a longer description of the change: (press enter to skip)\n',
-          default: options.defaultBody
-        }, {
-          type: 'confirm',
-          name: 'isBreaking',
-          message: 'Are there any breaking changes?',
-          default: false
-        }, {
-          type: 'input',
-          name: 'breaking',
-          message: 'Describe the breaking changes:\n',
-          when: function(answers) {
-            return answers.isBreaking;
-          }
-        }, {
-          type: 'confirm',
-          name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
-          default: options.defaultIssues ? true : false
-        }, {
-          type: 'input',
-          name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
-          when: function(answers) {
-            return answers.isIssueAffected;
-          },
-          default: options.defaultIssues ? options.defaultIssues : undefined
         }
       ]).then(function(answers) {
-
-        var maxLineWidth = 100;
-
-        var wrapOptions = {
-          trim: true,
-          newline: '\n',
-          indent:'',
-          width: maxLineWidth
-        };
-
-        // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
+        var maxLineWidth = 200;
         // Hard limit this line
         var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
-
-        // Wrap these lines at 100 characters
-        var body = wrap(answers.body, wrapOptions);
-
-        // Apply breaking change prefix, removing it if already present
-        var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
-        breaking = wrap(breaking, wrapOptions);
-
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
-
-        var footer = filter([ breaking, issues ]).join('\n\n');
-
-        commit(head + '\n\n' + body + '\n\n' + footer);
+        commit(head);
       });
     }
   };
